987102940536931dd776fafe54a1f5c52b556698
melon <530274554@qq.com>
2016-05-25 15:36:00 +0800

    navigation

--- app/src/main/java/com/melon/myapp/functions/ui/FragmentTabHostCustomTitleActivity.java
+++ app/src/main/java/com/melon/myapp/functions/ui/FragmentTabHostCustomTitleActivity.java
@@ -3,7 +3,10 @@ package com.melon.myapp.functions.ui;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.melon.myapp.BaseFragmentActivity;
 import com.melon.myapp.R;
@@ -11,6 +14,7 @@ import com.melon.myapp.functions.fragment.CommonFragment;
 
 public class FragmentTabHostCustomTitleActivity extends BaseFragmentActivity {
     private String[] titles = {"新闻", "音乐", "人生"};
+    private int[] imgs = {R.drawable.ic_menu_copy_holo_light,R.drawable.ic_menu_cut_holo_light,R.drawable.ic_menu_edit};
     private FragmentTabHost mTabHost = null;
 
     @Override
@@ -23,10 +27,25 @@ public class FragmentTabHostCustomTitleActivity extends BaseFragmentActivity {
     @Override
     protected void initData() {
         for (int i = 0; i < titles.length; i++) {
-            mTabHost.addTab(mTabHost.newTabSpec(i+"").setIndicator(titles[i]), CommonFragment.class, putContent(titles[i]));
+            mTabHost.addTab(mTabHost.newTabSpec(i+"").setIndicator(getTabTitleView(i)), CommonFragment.class, putContent(titles[i]));
+            //设置Tab按钮的背景
+            mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.drawable.selector_tab_item_title_bg);
         }
     }
+    /**
+     * 给Tab按钮设置图标和文字
+     */
+    private View getTabTitleView(int index){
+        View view = View.inflate(this,R.layout.tab_item_title_view, null);
 
+        ImageView imageView = (ImageView) view.findViewById(R.id.iv_tab_item_title);
+        imageView.setImageResource(imgs[index]);
+
+        TextView textView = (TextView) view.findViewById(R.id.tv_tab_item_title);
+        textView.setText(titles[index]);
+
+        return view;
+    }
     private Bundle putContent(String content) {
         Bundle bundle = new Bundle();
         bundle.putString("content", content);
