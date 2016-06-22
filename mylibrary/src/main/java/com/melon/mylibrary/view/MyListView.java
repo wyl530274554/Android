4c31643d2df9660c99fd000478764db48fc6db4f
melon <530274554@qq.com>
2016-06-22 16:13:35 +0800

    wx progress bar

--- mylibrary/src/main/java/com/melon/mylibrary/view/MyListView.java
+++ mylibrary/src/main/java/com/melon/mylibrary/view/MyListView.java
@@ -2,6 +2,7 @@ package com.melon.mylibrary.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.ListView;
 
@@ -12,42 +13,39 @@ import com.melon.mylibrary.util.LogUtils;
  * Created by mleon on 2016/6/14.
  */
 public class MyListView extends ListView{
-    int startY;
     private int screenHeight;
-
+    private GestureDetector mDetector;
     public MyListView(Context context, AttributeSet attrs) {
         super(context, attrs);
         if(screenHeight==0)
             screenHeight = CommonUtil.getScreenHeight(getContext());
+
+        mDetector = new GestureDetector(context,new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                scrollBy(0, (int) distanceY);
+                return super.onScroll(e1,e2,distanceX,distanceX);
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return super.onFling(e1, e2, velocityX, velocityY);
+            }
+        });
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-
         switch (ev.getAction()){
             case MotionEvent.ACTION_DOWN:
-                startY = (int) ev.getRawY();
-                LogUtils.e(startY+"");
-                break;
-            case MotionEvent.ACTION_MOVE:
-                int tempY = (int) ev.getRawY();
-                int dy = tempY-startY;
-
-                int newTop = this.getTop()+dy;
-                int  newBottom = this.getBottom()+dy;
-
-                LogUtils.e(newBottom+"---"+screenHeight+"---"+dy+"---"+this.getBottom());
-                //边界控制
-                if(newTop<0  || newBottom> screenHeight-40){
-                    break;
-                }
-                this.layout(this.getLeft(),newTop,this.getRight(),newBottom);
-
-                startY = (int) ev.getRawY();
                 break;
             case MotionEvent.ACTION_UP:
+                scrollTo(0,0);
+                break;
+            case MotionEvent.ACTION_MOVE:
                 break;
         }
+        mDetector.onTouchEvent(ev);
         return true;
     }
 }
