37c2b6adba9ff31ad2dcfeb51e23029babd29b6a
melon <530274554@qq.com>
2019-08-27 16:26:12 +0800

    demo

--- mylibrary/src/main/java/com/melon/mylibrary/Demo.java
+++ mylibrary/src/main/java/com/melon/mylibrary/Demo.java
@@ -1,5 +1,7 @@
 package com.melon.mylibrary;
 
+import java.sql.Timestamp;
+
 /**
  * Created by melon on 2017/9/26.
  * Email 530274554@qq.com
@@ -7,11 +9,14 @@ package com.melon.mylibrary;
 
 public class Demo {
     public static void main(String[] args){
-        String a = "kilimall";
-        String b = "kili"+"mall";
-        String c = new String("kilimall");
-        System.out.print(a==b);
-        System.out.print(c==b);
-        System.out.print(!a.equals(b));
+//        String a = "kilimall";
+//        String b = "kili"+"mall";
+//        String c = new String("kilimall");
+//        System.out.print(a==b);
+//        System.out.print(c==b);
+//        System.out.print(!a.equals(b));
+
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        System.out.println(ts.toString());
     }
 }
