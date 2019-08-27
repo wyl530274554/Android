f25a5fdbd523f0fdd2be2d3bb2469851984ef4a0
melon <530274554@qq.com>
2019-08-27 16:30:34 +0800

    demo

--- mylibrary/src/main/java/com/melon/mylibrary/Demo.java
+++ mylibrary/src/main/java/com/melon/mylibrary/Demo.java
@@ -16,7 +16,6 @@ public class Demo {
 //        System.out.print(c==b);
 //        System.out.print(!a.equals(b));
         Timestamp ts = new Timestamp(System.currentTimeMillis());
-
         System.out.println(ts.toString());
     }
 }
