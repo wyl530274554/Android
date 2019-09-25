99ddfdffd91f2e2c8aa22664f2f7ffa566def280
melon <530274554@qq.com>
2019-09-25 15:38:40 +0800

    test

--- app/src/main/java/com/melon/myapp/Constants.java
+++ app/src/main/java/com/melon/myapp/Constants.java
@@ -9,12 +9,10 @@ package com.melon.myapp;
 public class Constants {
     public static final String NET_PROTOCOL_HTTP = "http";
     public static final String PROTOCOL_TEL = "tel";
-
     public static final String JPUSH_AUTH = "Basic N2NlMTNjOWFjNmRlMDc4ODQ1MWVjZTRlOjQ0YzM3MDJjZjY1YTg0NTU5NzkzYWM2OQ==";
 
     public static final String URL_BAI_DU = "https://m.baidu.com/s?word=";
     public static final String URL_JD = "https://m.jd.com";
     public static final String URL_JPUSH_API = "https://api.jpush.cn/v3/push";
-
     public static final String BROADCAST_NEW_NOTIFY = "1000";
 }
