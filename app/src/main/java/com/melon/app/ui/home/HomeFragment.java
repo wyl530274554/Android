711283aa0907b70c3890fa43d385e41219c5d40f
melon <530274554@qq.com>
2020-01-27 20:29:41 +0800

    full screen video

--- app/src/main/java/com/melon/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/melon/app/ui/home/HomeFragment.java
@@ -1,9 +1,15 @@
 package com.melon.app.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +19,8 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.melon.app.R;
+import com.melon.app.ui.web.WebActivity;
+import com.melon.mylibrary.util.Constants;
 
 public class HomeFragment extends Fragment {
 
@@ -20,16 +28,46 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final EditText etSearch = root.findViewById(R.id.et_home_search);
+        final ImageView ivDel = root.findViewById(R.id.iv_home_del);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                etSearch.setText(s);
+            }
+        });
+
+        ivDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeViewModel.setText("");
+            }
+        });
+
+        etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    String content = etSearch.getText().toString().trim();
+                    if (!TextUtils.isEmpty(content)) {
+                        enterWeb(content);
+                    }
+                    return true;
+                }
+                return false;
             }
         });
         return root;
+    }
+
+    private void enterWeb(String content) {
+        if(!content.startsWith(Constants.NET_PROTOCOL_HTTP)) {
+            content = Constants.URL_BAI_DU + content;
+        }
+        Intent intent = new Intent(getActivity(), WebActivity.class);
+        intent.putExtra("url", content);
+        startActivity(intent);
     }
 }
