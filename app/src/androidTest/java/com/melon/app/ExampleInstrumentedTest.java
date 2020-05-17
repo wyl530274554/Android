3fceeba85715fa8a58b122caae39a75a38c8088a
Melon <530274554@qq.com>
2020-05-17 09:07:48 +0800

    right androidTest

--- app/src/androidTest/java/com/melon/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/melon/app/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("om.melon.app", appContext.getPackageName());
+        assertEquals("com.melon.app", appContext.getPackageName());
     }
 }
