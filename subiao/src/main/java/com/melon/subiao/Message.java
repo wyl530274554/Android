22be94dd3eba1eb41fe569deeef9433f76e75f91
wswangyl <wswangyl@sunnyoptical.com>
2020-12-01 14:29:31 +0800

    adas报警内容

--- subiao/src/main/java/com/melon/subiao/Message.java
+++ subiao/src/main/java/com/melon/subiao/Message.java
@@ -40,11 +40,46 @@ public class Message {
         sb.append("校验码").append(" : ").append(HexStringUtils.toHexString(new byte[]{unescapedMsg[1]})).append("\n");
         sb.append("流水号").append(" : ").append(HexStringUtils.toHexString(new byte[]{unescapedMsg[2], unescapedMsg[3]})).append("\n");
         sb.append("厂商编号").append(" : ").append(HexStringUtils.toHexString(new byte[]{unescapedMsg[4], unescapedMsg[5]})).append("\n");
-        sb.append("外设编号").append(" : ").append(HexStringUtils.toHexString(new byte[]{unescapedMsg[6]})).append("\n");
-        sb.append("功能码").append(" : ").append(HexStringUtils.toHexString(new byte[]{unescapedMsg[7]})).append("\n");
-        sb.append("数据内容").append(" : ").append(HexStringUtils.toHexString(BitOperator.splitBytes(msgBytes, 8, msgBytes.length - 2))).append("\n");
+
+        byte peripheralNumber = unescapedMsg[6];
+        sb.append("外设编号").append(" : ").append(HexStringUtils.toHexString(new byte[]{peripheralNumber})).append("\n");
+        byte functionCode = unescapedMsg[7];
+        sb.append("功能码").append(" : ").append(HexStringUtils.toHexString(new byte[]{functionCode})).append("\n");
+
+        byte[] dataBytes = BitOperator.splitBytes(msgBytes, 8, msgBytes.length - 2);
+        sb.append("数据内容").append(" : ").append(HexStringUtils.toHexString(dataBytes)).append("\n");
+        parseDataContent(sb, dataBytes, functionCode, peripheralNumber);
         sb.append("标识符").append(" : ").append("7E");
         return sb.toString();
+    }
+
+    private static void parseDataContent(StringBuilder sb, byte[] dataBytes, byte functionCode, byte peripheralNumber) {
+        if (functionCode == 0x36 && peripheralNumber == 0x64) {
+            //ADAS报警
+            if (dataBytes.length != 31 + dataBytes[30] * 5) {
+                sb.append("        ").append("内容长度不正确：").append(dataBytes.length).append("\n");
+                return;
+            }
+
+            sb.append("        ").append("报警ID：").append(BitOperator.byteToInteger(BitOperator.splitBytes(dataBytes, 0, 3))).append("\n");
+            sb.append("        ").append("标志状态：").append(dataBytes[4]).append("\n");
+            sb.append("        ").append("报警类型：").append(dataBytes[5]).append("\n");
+            sb.append("        ").append("前车车速：").append(dataBytes[6]).append("\n");
+            sb.append("        ").append("前车距离：").append(dataBytes[7]).append("\n");
+            sb.append("        ").append("偏离类型：").append(dataBytes[8]).append("\n");
+            sb.append("        ").append("道路标志类型：").append(dataBytes[9]).append("\n");
+            sb.append("        ").append("道路标志数据：").append(dataBytes[10]).append("\n");
+            sb.append("        ").append("车速：").append(dataBytes[11]).append("\n");
+            sb.append("        ").append("纬度：").append(BitOperator.byteToInteger(BitOperator.splitBytes(dataBytes, 14, 17))).append("\n");
+            sb.append("        ").append("经度：").append(BitOperator.byteToInteger(BitOperator.splitBytes(dataBytes, 18, 21))).append("\n");
+            sb.append("        ").append("时间：").append(HexStringUtils.toHexString(BitOperator.splitBytes(dataBytes, 22, 27))).append("\n");
+            sb.append("        ").append("车辆状态：").append(BitOperator.byteToInteger(BitOperator.splitBytes(dataBytes, 28, 29))).append("\n");
+            sb.append("        ").append("多媒体总数：").append(dataBytes[30]).append("\n");
+            for (int i = 0; i < dataBytes[30]; i++) {
+                sb.append("        ").append("多媒体类型：").append(dataBytes[31 + i * 5]).append(", ").append("多媒体ID: ").append(BitOperator.byteToInteger(BitOperator.splitBytes(dataBytes, 31 + i * 5 + 1, 31 + i * 5 + 4))).append("\n");
+            }
+            return;
+        }
     }
 
     private static int checkCodeError(byte[] contents) {
